6206d7c7f94ce45c3540f8833c1843e8603240a0
WeiqinDing <44372155+WeiqinDing@users.noreply.github.com>
2018-11-19 12:46:15 +0000

    Add files via upload

---
+++ project-app-api/create.js
@@ -0,0 +1,40 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure} from "./libs/response-lib";
+
+
+export async function main(event, context) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "notes",
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            // I changed the Item here, because my table
+            // is different to the notes table
+            // userId: event.requestContext.identity.cognitoIdentityId,
+            // projectId: uuid.v1(),
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        console.log(e);
+        return failure({ status : false});
+    }
+}
---
+++ project-app-api/createUserRole.js
@@ -0,0 +1,29 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure} from "./libs/response-lib";
+
+
+export async function main(event, context) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "userRole",
+
+        Item: {
+            // userId: event.requestContext.identity.cognitoIdentityId,
+            // projectId: uuid.v1(),
+            userID: data.userID,
+            userRole: data.userRole
+
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        console.log(e);
+        return failure({ status : false});
+    }
+}
